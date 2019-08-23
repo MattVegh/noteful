e2d46d8b1b748d01404dd5ff60c0b40784f577fc
Matt Vegh <mdv240@gmail.com>
2019-08-23 17:14:50 -0500

    commit

--- src/Components/NoteList/NoteList.js
+++ src/Components/NoteList/NoteList.js
@@ -4,16 +4,18 @@ import './NoteList.css'
 import { Link } from 'react-router-dom'
 
 export default function NoteList(props) {
+     const note = props.notes.filter(note => note.folderId === props.match.params.folderId);
+     
     return (
         <section className='NoteList'>
             <ul>
-            {props.notes.map(note =>
+            {/* {props.notes.map(note =>
                  <li key={note.id}>
-                    <Link to={`folder/folderId/${note.id}`}  className='NoteHeader'>{note.name}</Link>
+                    <Link to={`folder/:folderId/${note.id}`}  className='NoteHeader'>{note.name}</Link>
                     <p className='NoteDate'>{note.modified}</p>
                     <button className='DeleteNoteButton'>Delete Note</button>
-                </li>)}
-                
+                </li>)} */}
+                {props.notes.map(note => <li>{note}</li>)}
             </ul>
             <Link to='/AddNote'>Add Note</Link>
         </section>
